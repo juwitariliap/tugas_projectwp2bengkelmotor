9a77d09173094e2fc73758be5ab1dc060cd21094
juwitariliap <juwitaputri1212@gmail.com>
2024-11-02 19:52:21 +0700

    tugas-wpproject

---
+++ resources/views/about.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>page about</title>
+
+    <style>
+
+nav {
+    background: rgb(59, 159, 193);
+    padding: 15px 0px;
+    display: grid;
+    place-items: center;
+}
+
+nav > div {
+    font-size: 1.3em;
+    width: 40%;
+    display: flex;
+    justify-content: space-around;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
+
+section > div {
+    margin: auto;
+    width: 45%;
+}
+
+    </style>
+</head>
+<body>
+    
+    <nav>
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/blog">Blog</a>
+            <a href="/contact">contact</a>
+        </div>
+    </nav>
+   
+    <br><br>
+
+
+    <section>
+        <div>
+            <h1 style="text-align: center;">Tentang Universitas BSI</h1>
+            <p style="font-size: 1.2em; text-align: justify; padding-top: 15px;"><b>Kuliah…? BSI Aja!!</b> Slogan ini pasti sudah melekat di hati dan pikiran sebagian besar masyarakat di Indonesia. Tentunya hal ini mendorong BSI (Bina Sarana Informatika) untuk selalu memperbaiki diri di segala bidang.
+
+                Tentang Kami
+
+Selamat datang di Solo Motor, bengkel motor terpercaya di Solo yang mengutamakan pelayanan terbaik dan kualitas. Kami hadir untuk memenuhi kebutuhan perawatan, perbaikan, serta modifikasi motor Anda dengan standar profesional.
+
+Kenapa Memilih Solo Motor?
+
+Teknisi Berpengalaman: Tim kami terdiri dari teknisi berpengalaman yang siap memberikan solusi terbaik untuk motor Anda.
+Peralatan Lengkap dan Modern: Kami menggunakan peralatan terkini yang membantu memastikan perawatan dan perbaikan dilakukan dengan presisi dan efisiensi.
+Pelayanan Ramah dan Cepat: Solo Motor selalu mengedepankan kepuasan pelanggan dengan pelayanan yang ramah dan cepat.
+Harga Transparan: Kami menyediakan layanan dengan harga yang transparan dan kompetitif, tanpa biaya tersembunyi.
+Layanan Kami
+
+Solo Motor menawarkan berbagai layanan, termasuk:
+
+Servis Rutin: Perawatan berkala untuk menjaga performa motor Anda tetap prima.
+Perbaikan Mesin: Mengatasi berbagai masalah mesin dengan akurat dan cepat.
+Modifikasi dan Custom: Layanan modifikasi untuk mengubah tampilan atau performa motor sesuai keinginan Anda.
+Suku Cadang Berkualitas: Kami hanya menggunakan suku cadang berkualitas yang terjamin keasliannya.
+    </section>
+</body>
+</html>
